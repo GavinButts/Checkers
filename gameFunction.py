1455727628dbccaaa8cba514c4a0a541d980c738
GavinButts <97565155+GavinButts@users.noreply.github.com>
2023-02-16 12:21:52 -0800

    Add tasks to complete

---
+++ gameFunction.py
@@ -0,0 +1,19 @@
+## TO DO
+# 1. Create a game board class that is implemented through a 2D array
+#          This class should be instantiated with player's pieces in starting position
+#          This class should register where the pieces are
+#          This class should be able to determine win conditions
+#
+# 2. Create a player class
+#          This class should have player's color, player's turn, etc. as fields
+#          This class should be able to allow the player to move a piece
+#          This class should determine if a player's move is valid
+#
+# 3. Create game visuals
+#          There should be player piece visuals
+#          There should be game board visuals
+#          There should be a title screen visuals
+#
+# 4. Implement a title screen with adjustments and options
+#
+# 5. Choose audio to accompany actions and a sound track for the game
